afd7724ba29a184c3ad651ab01ff7bf848e0705f
Guillaume Gomez <guillaume1.gomez@gmail.com>
2018-11-14 00:30:27 +0100

    Prevent CVE

--- ultron/setup.py
+++ ultron/setup.py
@@ -8,6 +8,6 @@ setup(
         'Programming Language :: Python :: 3.5',
     ],
     install_requires=['grequests>=0.3,<0.4',
-                      'requests>=2.11,<2.12',
+                      'requests>=2.20',
                       'watchdog>=0.8,<0.9'],
 )
